3e7aa63e3939ff6b9aeea4d702386fb9e78c9909
Skitaarii <166595524+Skitaarii@users.noreply.github.com>
2026-03-22 02:08:29 +0100

    Prep stuff cause Dyumes forgor to give me shop

--- scenes/game/shops/Slot.gd
+++ scenes/game/shops/Slot.gd
@@ -1,6 +1,7 @@
 extends PanelContainer
 class_name Slot
 
+@onready var player = get_tree().get_first_node_in_group("player")
 @onready var texture_rect = $TextureRect
 @export var item : Item = null:
 	set(value):
@@ -24,13 +25,44 @@ func get_preview():
 	return preview
 	
 func _can_drop_data(_pos, data):
-	return data is Slot
+	var source = data.get_parent().name
+	var destination = get_parent().name
+	
+	if data is Slot:
+		if destination == "Shop" and source == "Inventory" and not item:
+			return true
+		elif destination == "Inventory" and source == "Shop" and player.moneyIndicator >= data.item.cost and not item:
+			return true
+		elif destination == source:
+			return true
+			
+	return false
 
 func _get_drag_data(_at_position):
 	set_drag_preview(get_preview())
 	return self
 	
 func _drop_data(_at_position, data):
+	var source = data.get_parent().name
+	var destination = get_parent().name
+	
+	if destination == "Shop" and source == "Inventory":
+		selling(data)
+		return
+	elif destination == "Inventory" and source == "Shop":
+		buying(data)
+		return
+	
 	var temp = item
 	item = data.item
 	data.item = temp
+
+func selling(data):
+	print("Sold " + data.item.name)
+	player.moneyIndicator += data.item.cost
+	data.item = null
+
+func buying(data):
+	print("Bought " + data.item.name)
+	player.moneyIndicator -= data.item.cost
+	item = data.item
